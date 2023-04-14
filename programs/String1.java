66d28f83a82e293fe4d733cfb43306eb321c3a73
samruddha kumbhar <64959366+sam1231ui@users.noreply.github.com>
2023-04-14 20:59:50 +0530

    Update String1.java

--- programs/String1.java
+++ programs/String1.java
@@ -2,7 +2,7 @@ package programs;
 
 public class String1 {
 	public static void main(String []args) {
-			
+			//program to print string char wise
 			String str = "welcome";
 			for(int i=0; i<str.length(); i++) {
 				
